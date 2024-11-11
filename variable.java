7913f3bf1fcbadb023b934d211d9d0760a755a5d
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2024-11-11 11:43:09 +0600

    Use Of Variable

---
+++ variable.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+
+public class variable
+{
+	public static void main (String args[])
+	{
+		boolean b;
+		b = true;
+		int i;
+		i = 24565301;
+		float f;
+		f = 420.2f;
+		double d;
+		d = 89.924321;
+		System.out.println("Value of b is: " +b);
+		System.out.println("Value of i is: " +i);
+		System.out.println("Value of f is: " +f);
+		System.out.println("Value of d is: " +d);
+	
+	}
+}

0d8a89e17e37cb3e6aa4c3cc3500c1505b4fd615
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2024-11-11 11:42:32 +0600

    Non Premitted To Premited Conversion

---
+++ nonPremittedToPremited.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class nonPremittedToPremited
+{
+	public static void main (String []args)
+	{
+		String str = "123";
+		String str2 = "127";
+		System.out.println(str+str2);
+		int value = Integer.parseInt(str);
+		int value2 = Integer.parseInt(str2);
+		System.out.println(value+value2);
+	}
+}

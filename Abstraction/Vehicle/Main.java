ff79facc58d9ebea45f29e6e7e513a36c59f75e4
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2025-01-12 20:29:21 +0600

    Main file of this code

---
+++ Abstraction/Vehicle/Main.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		MultiVehicle m = new Car("BMW","i4 eDrive35 Gran Coupe","Brake discs and pads");
+		m.ShowDetails();
+	}
+}

ca92ce83a752202a251f426139c0a33e8fc3c011
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2025-10-20 18:10:48 +0600

    How to show Biodata of persons

---
+++ BioData.java
@@ -0,0 +1,13 @@
+
+public class BioData {
+    
+   public BioData() 
+   {
+
+   }
+
+   public static void main(String[] args) {
+      System.out.println("Ankur Basak");
+      System.out.println("01717257250");
+   };
+};

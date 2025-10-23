31ce2c87fd63ce625bb39a933e5d20c7bb691b2a
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2025-10-23 13:01:41 +0600

    Showing details of a product

---
+++ Product.java
@@ -0,0 +1,17 @@
+public class Product {
+    
+    public Product()
+    {
+
+    }
+
+    public static void main(String[]args)
+    {
+        System.out.println("ID: 101");
+        System.out.println("Title: iphone15");
+        System.out.println("Price: 1895 euros");
+        System.out.println("Description: Perfect product with bestb image quality");
+        System.out.println("Catagory: Phone");
+
+    };
+};

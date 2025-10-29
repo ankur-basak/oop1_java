41394bb06a6d02003e68e52f9e930ac7d349e0b8
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2025-10-29 22:38:27 +0600

    calculation System of every type of math

---
+++ MathDemo.java
@@ -0,0 +1,16 @@
+public class MathDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-9.7));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.pow(3,5));
+        System.out.println(Math.PI);
+        System.out.println(Math.log(2));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.max(-2.7, 8.15));
+        System.out.println(Math.min(-2.7, 8.15));
+        System.out.println(Math.ceil(-5.7));
+        System.out.println(Math.floor(-5.8));
+    }
+}

b25de7b973bd98fd9e85d81246047d880766e027
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2025-10-30 00:50:32 +0600

    how to summation of numbers without variables  length declared

---
+++ Variable_Length_Arguments/variable_length_arguments.java
@@ -0,0 +1,11 @@
+public class variable_length_arguments {
+
+     void add(int... num){
+
+        int sum = 0;
+        for (int x : num){
+            sum = sum + x;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ Variable_Length_Arguments/variable_length_argumentsStart.java
@@ -0,0 +1,11 @@
+public class variable_length_argumentsStart {
+
+    public static void main(String[] args) {
+        
+        variable_length_arguments ob = new variable_length_arguments();
+        ob.add(5,25);
+        ob.add(12,21,15,35,65);
+        ob.add(12,15,35,65);
+    }
+    
+}

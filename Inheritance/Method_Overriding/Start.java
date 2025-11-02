3d6de7e7b18bbb5abecafdc28c6f91192961f268
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2025-11-03 01:29:39 +0600

    Method Overloading

---
+++ Inheritance/Method_Overriding/Start.java
@@ -0,0 +1,16 @@
+public class Start {
+
+    public static void main(String[] args) {
+        
+        Teacher t1 = new Teacher();
+        t1.name = "Ankur Basak";
+        t1.age = 22;
+        t1.qualification = "B.Tech in CSE";
+        t1.displayInformation();
+
+        Person p1 = new Person();
+        p1.name = "Aranb Basak";
+        p1.age = 26;
+        p1.displayInformation();
+    }
+}
